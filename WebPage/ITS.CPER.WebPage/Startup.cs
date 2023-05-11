876b0b4774236facecbec0089c573748399669c2
Paolo Gattesco <paolo.gattesco@yoroi.company>
2023-05-11 11:46:18 +0200

    clean floders

---
+++ WebPage/ITS.CPER.WebPage/Startup.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using ITS.CPER.WebPage.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting.Internal;
+
+namespace ITS.CPER.WebPage;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
+    {
+        Configuration = configuration;
+        HostingEnvironment = hostingEnvironment;
+    }
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment HostingEnvironment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddSingleton<WeatherForecastService>();
+
+        services.AddAuthentication(options => {
+            options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddOpenIdConnect("Auth0", options => {
+            options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+            options.ClientId = Configuration["Auth0:ClientId"];
+            options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+            options.ResponseType = OpenIdConnectResponseType.Code;
+
+            options.Scope.Clear();
+            options.Scope.Add("openid");
+            options.Scope.Add("profile"); // <- Optional extra
+            options.Scope.Add("email");   // <- Optional extra
+
+            options.CallbackPath = new PathString("/callback");
+            options.ClaimsIssuer = "Auth0";
+            options.SaveTokens = true;
+
+            // Add handling of lo
+            options.Events = new OpenIdConnectEvents
+            {
+                OnRedirectToIdentityProviderForSignOut = (context) =>
+                {
+                    var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                    var postLogoutUri = context.Properties.RedirectUri;
+                    if (!string.IsNullOrEmpty(postLogoutUri))
+                    {
+                        if (postLogoutUri.StartsWith("/"))
+                        {
+                            var request = context.Request;
+                            postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                        }
+                        logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+                    }
+
+                    context.Response.Redirect(logoutUri);
+                    context.HandleResponse();
+
+                    return Task.CompletedTask;
+                }
+            };
+        });
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication(); // <- Add this
+        app.UseAuthorization();  // <- Add this
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();  // <- Add this
+            endpoints.MapBlazorHub();
+            endpoints.MapFallbackToPage("/_Host");
+        });
+    }
+}
