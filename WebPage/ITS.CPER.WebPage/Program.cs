09a7e1418a57eaa61f474e49f340153e3c54c91a
Paolo Gattesco <paolo.gattesco@yoroi.company>
2023-05-17 17:28:35 +0200

    dev: implemented users db

--- WebPage/ITS.CPER.WebPage/Program.cs
+++ WebPage/ITS.CPER.WebPage/Program.cs
@@ -11,7 +11,7 @@ using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
