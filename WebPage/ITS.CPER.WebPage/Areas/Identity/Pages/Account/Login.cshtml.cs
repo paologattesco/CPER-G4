0e6c1bae6cffe8c2c26e3bab79051660e2df7d51
Paolo Gattesco <paolo.gattesco@yoroi.company>
2023-05-19 12:26:51 +0200

    enh: login and register pages

--- WebPage/ITS.CPER.WebPage/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WebPage/ITS.CPER.WebPage/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -60,13 +60,10 @@ namespace ITS.CPER.WebPage.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -80,7 +77,7 @@ namespace ITS.CPER.WebPage.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember me")]
             public bool RememberMe { get; set; }
         }
 
@@ -111,7 +108,7 @@ namespace ITS.CPER.WebPage.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
