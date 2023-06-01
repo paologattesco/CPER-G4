1c23b0f829c1fa2114e0a67cd0a1dd1b3cc8d5af
Paolo Gattesco <paolo.gattesco@yoroi.company>
2023-06-01 12:51:02 +0200

    enh: changed session id

---
+++ WebPage/ITS.CPER.WebPage/Data/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace ITS.CPER.WebPage.Data.Models
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Country_Code { get; set; }
+        public string Suburb { get; set; }
+        public string Road { get; set; }
+        public string State_District { get; set; }
+    }
+}
